f440f87ce6447c2232a5aafe245baed7e9e7e6a8
Cynthia <wangxi.cynthia1@gmail.com>
2019-02-28 21:46:30 -0700

    Changed the layout. Added timestamp.

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
 app.use(express.static('public'));
 
-app.get('/', function(req, res){
-    // res.sendFile(__dirname + '/index.html');
-
-});
+io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+        let timestamp = Date.now();  // get the timestamp when received new msg
+        io.emit('chat message', timestamp, msg);
+        console.log(timestamp);
+    })
+    socket.on('disconnect', function (){
+        console.log('user disconnected');
+    });
+})
 
 http.listen(3000, function(){
     console.log('listening on *:3000');
