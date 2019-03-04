9a442dcea173f2423c89e0d770927eb0f80252da
Cynthia <wangxi.cynthia1@gmail.com>
2019-03-03 17:57:05 -0700

    Almost done.

--- index.js
+++ index.js
@@ -4,15 +4,34 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var totalUserCount = 0;
 var users = [];
+var chatLog = [];
 
 app.use(express.static('public'));
 
-io.on('connection', function(socket){
+function findUserBySocket(socketid) {
+    for(i=0; i<users.length; i++){
+        if(users[i].id === socketid){
+            return i;
+        }
+    }
+    return -1;
+}
+
+function isUniqueName(name) {
+    for(i=0; i<users.length; i++){
+        if(name === users[i].username){
+            return false;
+        }
+    }
+    return true;
+}
+
+io.on('connection', function(socket) {
 
     // New User
+    let rdmcolor = "hsl(" + (totalUserCount * 77 % 360) + ",100%,50%)";
     totalUserCount ++;
     let nickname = "User#" + totalUserCount.toString();
-    let rdmcolor = "hsl(" + (totalUserCount * 47 % 360) + ",100%,50%)"
     users.push({
         username: nickname,
         color: rdmcolor,
@@ -21,27 +40,72 @@ io.on('connection', function(socket){
     console.log(nickname + " has connected");
     socket.emit('assign nickname', {username: nickname, color: rdmcolor});
     io.emit('update user list', users);
-    //TODO: send chat log to new user
+    // Send chat log to new user
+    socket.emit('new chatLog', chatLog);
 
     // Server received new message from clients
     socket.on('chat message', function(data){
-        //TODO: check msg for name changing and color changing
         if(data.trim().length === 0) return; // Don't send empty msg
         let timestamp = Date.now();  // get the timestamp when received new msg
-        io.emit('new message', {time: timestamp, msg: data});
-    })
-    socket.on('disconnect', function (){
-        // Loop through the user list to match socket id
-        for(i=0; i<users.length; i++){
-            if(users[i].id === socket.id){
-                console.log(users[i].username + ' has disconnected');
-                users.splice(i, 1);
-                break;
+        let user_index = findUserBySocket(socket.id);
+        let user = users[user_index];
+
+        // Check if it's /nick or /nickcolor
+        let command = data.split(" ");
+        if(command.length === 2 && command[0] === "/nick"){
+            if(command[1].trim().length === 0){ // Invalid empty name
+                socket.emit('command reply', "[Error] Invalid nickname: nickname cannot be empty.");
+                return;
             }
+            // Check if the new name is unique
+            if(!isUniqueName(command[1])){
+                socket.emit('command reply', "[Error] Invalid nickname: not unique.");
+                return;
+            }
+            // Change the name
+            let oldname = users[user_index].username;
+            users[user_index].username = command[1];
+            socket.emit('assign nickname', {username: command[1], color: users[user_index].color});
+            // Update user list
+            io.emit('update user list', users);
+            // Update chatLog
+            for(i=0; i<chatLog.length; i++){
+                if(chatLog[i].username === oldname){
+                    chatLog[i].username = command[1];
+                    console.log(i);
+                }
+            }
+            io.emit('update chatLog', chatLog);
+            // Emit success message
+            socket.emit('command reply', "[Done] Successfully changed nickname.");
+        }else if(command.length === 2 && command[0] === "/nickcolor"){
+            if(parseInt("0x" + command[1]) > 0xFFFFFF){
+                socket.emit('command reply', "[Error] Invalid Hex code: color should be RRGGBB.");
+                return;
+            }
+            socket.emit('command reply', "[Done] Successfully changed color.");
+        }else{
+            let msgWrap = {
+                time: timestamp,
+                msg: data,
+                username: user.username,
+                color: user.color
+            };
+            chatLog.push(msgWrap);
+            io.emit('new message', msgWrap);
         }
-        io.emit('update user list', users);
     });
-})
+
+    // User disconnected
+    socket.on('disconnect', function (){
+        let index = findUserBySocket(socket.id);
+        if(index !== -1){
+            console.log(users[i].username + ' has disconnected');
+            users.splice(i, 1);
+            io.emit('update user list', users);
+        }
+    });
+});
 
 http.listen(3000, function(){
     console.log('listening on *:3000');
--- public/client.js
+++ public/client.js
@@ -3,34 +3,72 @@ $(function () {
     let nickname = "default";
     let $chatHistory =  $('#chat-history');
     let $onlineUsers = $('#online-users');
+    let $msgInput = $('#msg-input');
 
     $('form').submit(function(e){
         e.preventDefault(); // prevent reloading page
-        socket.emit('chat message', $('#msg-input').val());
-        $('#msg-input').val('');
+        socket.emit('chat message', $msgInput.val());
+        $msgInput.val('');
         return false;
     });
-    socket.on('new message', function(data){
-        let date = new Date(data.time);
+    socket.on('new message', function(msgWrap){
+        let date = new Date(msgWrap.time);
         let date_str = date.toTimeString().split(" ");
-        let display_msg = date_str[0] + " " + nickname + ": " + data.msg;
-        $chatHistory.prepend("<li>" + date_str[0] + " " +
-            "<a style='color: " + "red" + "'>" + nickname + "</a>" + ": " +
-            data.msg + "</li>");
+        if(msgWrap.username !== nickname){
+            $chatHistory.prepend("<li>" + date_str[0] + " " +
+                "<a style='color: " + msgWrap.color + "'>" + msgWrap.username + "</a>" + ": " +
+                msgWrap.msg + "</li>");
+        }else{  // bold the message
+            $chatHistory.prepend("<li><b>" + date_str[0] + " " +
+                "<a style='color: " + msgWrap.color + "'>" + msgWrap.username + "</a>" + ": " +
+                msgWrap.msg + "</b></li>");
+        }
+
     });
 
     socket.on('assign nickname', function(data){
         nickname = data.username;
-        $('#welcome-msg')[0].innerText = ("Welcome, " + nickname);
-        $('#welcome-msg').css({'color': data.color});
-        console.log(data.color);
+        $('#welcome-msg')[0].innerHTML = "Welcome, " + "<a style='color: " + data.color + "'>" +
+            data.username + "</a>";
     });
 
     socket.on('update user list', function(data){
-        $('#online-users').empty();
+       $onlineUsers.empty();
        for(i=0; i<data.length; i++){
            $onlineUsers.prepend("<li style='color: " + data[i].color +
                "'>" + data[i].username + "</li>" );
        }
     });
+
+    socket.on('new chatLog', function(chatLog){
+        $chatHistory.empty();
+        for(i=0; i<chatLog.length; i++){
+            let date = new Date(chatLog[i].time);
+            let date_str = date.toTimeString().split(" ");
+            $chatHistory.prepend("<li>" + date_str[0] + " " +
+                "<a style='color: " + chatLog[i].color + "'>" + chatLog[i].username + "</a>" + ": " +
+                chatLog[i].msg + "</li>");
+        }
+    });
+
+    socket.on('update chatLog', function(chatLog){
+        $chatHistory.empty();
+        for(i=0; i<chatLog.length; i++){
+            let date = new Date(chatLog[i].time);
+            let date_str = date.toTimeString().split(" ");
+            if(chatLog[i].username !== nickname) {
+                $chatHistory.prepend("<li>" + date_str[0] + " " +
+                    "<a style='color: " + chatLog[i].color + "'>" + chatLog[i].username + "</a>" + ": " +
+                    chatLog[i].msg + "</li>");
+            }else{  // bold the message
+                $chatHistory.prepend("<li><b>" + date_str[0] + " " +
+                    "<a style='color: " + chatLog[i].color + "'>" + chatLog[i].username + "</a>" + ": " +
+                    chatLog[i].msg + "</b></li>");
+            }
+        }
+    });
+
+    socket.on('command reply', function(data){
+        $chatHistory.prepend("<li>Server: " + data + "</li>");
+    })
 });
